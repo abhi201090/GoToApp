515d9fadfa59d10720a937a2e1db7bce8c50f5ce
sayansaha1143 <sayan.saha1143@gmail.com>
2017-01-28 16:57:00 -0500

    Add task modal

---
+++ GoToDo/www/js/taskController.js
@@ -0,0 +1,30 @@
+﻿app.controller('taskController', function ($scope, $ionicModal, $ionicPopup) {
+
+    // Create and load the Modal
+    $ionicModal.fromTemplateUrl('new-task.html', function (modal) {
+        $scope.taskModal = modal;
+    }, {
+        scope: $scope,
+        animation: 'slide-in-up'
+    });
+
+    // Open our new task modal
+    $scope.newTask = function () {
+        $scope.taskModal.show();
+    };
+
+    // Close the new task modal
+    $scope.closeNewTask = function () {
+        $scope.taskModal.hide();
+    };
+
+    $scope.selectedPriority = {}
+    $scope.priorities = [{
+        name: "High Priority"
+    }, {
+        name: "Medium Priority"
+    }, {
+        name: "Low Priority"
+    }]
+
+});
