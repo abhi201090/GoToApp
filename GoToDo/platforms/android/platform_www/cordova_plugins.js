58fbd3559739d7e5895b54bf7f80948346407c4b
HEGDEA15 <abhi201090@gmail.com>
2017-02-01 13:20:22 -0500

    Date Picker fixed

--- GoToDo/platforms/android/platform_www/cordova_plugins.js
+++ GoToDo/platforms/android/platform_www/cordova_plugins.js
@@ -1,6 +1,13 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
+        "file": "plugins/cordova-plugin-datepicker/www/android/DatePicker.js",
+        "id": "cordova-plugin-datepicker.DatePicker",
+        "clobbers": [
+            "datePicker"
+        ]
+    },
+    {
         "file": "plugins/cordova-plugin-device/www/device.js",
         "id": "cordova-plugin-device.device",
         "clobbers": [
@@ -22,14 +29,6 @@ module.exports = [
         ]
     },
     {
-        "file": "plugins/ionic-plugin-keyboard/www/android/keyboard.js",
-        "id": "ionic-plugin-keyboard.keyboard",
-        "clobbers": [
-            "cordova.plugins.Keyboard"
-        ],
-        "runs": true
-    },
-    {
         "file": "plugins/cordova-sqlite-storage/www/SQLitePlugin.js",
         "id": "cordova-sqlite-storage.SQLitePlugin",
         "clobbers": [
@@ -37,24 +36,25 @@ module.exports = [
         ]
     },
     {
-        "file": "plugins/cordova-plugin-datepicker/www/android/DatePicker.js",
-        "id": "cordova-plugin-datepicker.DatePicker",
+        "file": "plugins/ionic-plugin-keyboard/www/android/keyboard.js",
+        "id": "ionic-plugin-keyboard.keyboard",
         "clobbers": [
-            "datePicker"
-        ]
+            "cordova.plugins.Keyboard"
+        ],
+        "runs": true
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
     "cordova-plugin-console": "1.0.5",
+    "cordova-plugin-datepicker": "0.9.2",
     "cordova-plugin-device": "1.1.4",
     "cordova-plugin-splashscreen": "3.1.0",
     "cordova-plugin-statusbar": "2.1.3",
     "cordova-plugin-whitelist": "1.2.2",
-    "ionic-plugin-keyboard": "2.1.0",
     "cordova-sqlite-storage": "2.0.1",
-    "cordova-plugin-datepicker": "0.9.2"
+    "ionic-plugin-keyboard": "2.1.0"
 };
 // BOTTOM OF METADATA
 });
--- GoToDo/www/js/addTaskController.js
+++ GoToDo/www/js/addTaskController.js
@@ -1,21 +1,23 @@
-﻿app.controller('addTaskController', function ($scope, $cordovaDatePicker) {
+﻿app.controller('addTaskController', function ($scope, $cordovaDatePicker,$ionicPlatform) {
+    $scope.selectedDate = "Add a date"
+    $ionicPlatform.ready(function () {
+        $scope.showDatePicker = function () {
+            var options = {
+                date: new Date(),
+                mode: 'datetime', // or 'time'
+                minDate: new Date() - 10000,
+                allowOldDates: false,
+                allowFutureDates: true,
+                doneButtonLabel: 'DONE',
+                doneButtonColor: '#F2F3F4',
+                cancelButtonLabel: 'CANCEL',
+                cancelButtonColor: '#000000'
+            };
 
-    $scope.datePickerShow = function () {
-        var options = {
-            date: new Date(),
-            mode: 'datetime', // 'date' or 'time'
-            minDate: new Date(),
-            allowOldDates: false,
-            allowFutureDates: true,
-            doneButtonLabel: 'DONE',
-            doneButtonColor: '#F2F3F4',
-            cancelButtonLabel: 'CANCEL',
-            cancelButtonColor: '#000000'
+            $cordovaDatePicker.show(options).then(function (date) {
+                
+            });
         };
-        $cordovaDatePicker.show(options).then(function (date) {
-            $modelValue[$options.key] = date;
-        });
-    }
-
+    });
 
 });
